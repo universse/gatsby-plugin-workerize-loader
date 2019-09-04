32116572303f1e07fd287eb8d705a0eaf5130f1c
Ulrjch <phuoc317049@gmail.com>
2019-09-04 21:11:08 +0800

    Fix bug

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,13 +1,16 @@
-const { resolve } = require("path");
+const { resolve } = require('path')
 
-module.exports = ({ actions: { replaceWebpackConfig }, getConfig }) => {
-  const config = getConfig();
+exports.onCreateWebpackConfig = ({
+  actions: { replaceWebpackConfig },
+  getConfig
+}) => {
+  const config = getConfig()
 
   config.module.rules.push({
     test: /\.worker\.js$/,
-    use: { loader: "workerize-loader" }
-  });
-  config.output.globalObject = "this";
+    use: { loader: 'workerize-loader' }
+  })
+  config.output.globalObject = 'this'
 
-  replaceWebpackConfig(config);
-};
+  replaceWebpackConfig(config)
+}
