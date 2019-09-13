19504e8d08d5021038b31d02390af424db72b392
Ulrjch <phuoc317049@gmail.com>
2019-09-13 13:38:47 +0800

    Optimize ssr

--- src/gatsby-ssr.js
+++ src/gatsby-ssr.js
@@ -9,12 +9,24 @@ const isWorker = file => file.includes(prefix) && file.endsWith('.worker.js')
 const shouldPreload = (file, preloads) =>
   file.startsWith(prefix) && preloads.some(preload => file.includes(preload))
 
+  let preloadScripts = []
+
 exports.onRenderBody = ({ setHeadComponents }, { preloads = [] } = {}) => {
-  setHeadComponents(
-    readdirSync(publicFolder)
+  if (!preloads.length) return
+
+  if (!preloadScripts.length) {
+    preloadScripts = readdirSync(publicFolder)
       .filter(file => isWorker(file) && shouldPreload(file, preloads))
       .map(file => (
-        <link key={file} as='script' href={`/${file}`} rel='preload' />
+        <link
+          key={file}
+          as='javascript'
+          crossOrigin='anonymous'
+          href={`/${file}`}
+          rel='preload'
+        />
       ))
-  )
+  }
+
+  setHeadComponents(preloadScripts)
 }
